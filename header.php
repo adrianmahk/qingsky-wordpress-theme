43eb4911442d0adad9d2931f21682e7fefe9a4e0
adrianmahk@qingsky <qingskys@gmail.com>
2022-03-12 01:27:04 +0800

    [header] adding correct path to font

--- header.php
+++ header.php
@@ -29,6 +29,15 @@
 
 		}
 	</script>
+	<style>
+		@font-face {
+			font-family: 'Roboto';
+			font-style: italic;
+			font-weight: 300;
+			src: url(<?php echo get_template_directory_uri().'/assets/roboto.woff2'?>) format('woff2');
+			unicode-range: U + 0000-00FF, U + 0131, U + 0152-0153, U + 02BB-02BC, U + 02C6, U + 02DA, U + 02DC, U + 2000-206F, U + 2074, U + 20AC, U + 2122, U + 2191, U + 2193, U + 2212, U + 2215, U + FEFF, U + FFFD;
+		}
+	</style>
 	<script src="<?php echo get_template_directory_uri() . '/js/blog-ui-ajax.js' ?>" type="text/javascript"> </script>
 	
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
