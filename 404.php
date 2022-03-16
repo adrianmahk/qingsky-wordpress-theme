f6a8475bb96018f0e1f3b6855559bafd6c2b692a
adrianmahk@qingsky <qingskys@gmail.com>
2022-03-17 00:51:20 +0800

    [404] added refer link to old Blogger site [ignore] delete and ignore reference files

--- 404.php
+++ 404.php
@@ -21,6 +21,14 @@ get_header(); ?>
 <div class="post-outer-container">
 <div class="no-posts-message">
 找不到相符的結果
+<?php
+	if (strpos($_SERVER['REQUEST_URI'], '.html') > 0) {
+		echo '<br />註：本站在 2022 年 3 月搬家，搬運仍在進行中，鏈結可能失效，這裡可能有你想找的東西：<br />';
+		$url = 'https://blogger.qingsky.hk' . $_SERVER['REQUEST_URI'];
+		echo '<a href="' . $url . '">' . $url . '</a><br />' ;
+		echo '（此為本站的 Blogger 舊版本的鏈結）';
+	}
+?>
 </div>
 </div>
 </div>
