105423ed2c36e37d4a95c09817268777c78c26fe
adrianmahk@qingsky <qingskys@gmail.com>
2022-03-19 23:53:45 +0800

    [functions] even more stupid method to find srcset

--- functions.php
+++ functions.php
@@ -823,10 +823,16 @@ function clear_br($content) {
 	// for me to have imgs shown in both localhost and prod
 	foreach($html->find('img') as $element) {
     	$element->src = str_replace('http://' . $domain, '', str_replace('https://' . $domain, '', $element->src));
-	//    echo attachment_url_to_postid('https://' . $domain . $element->parent->href);
-		$id = attachment_url_to_postid('https://' . $domain . $element->parent->href);
-		
-	    $element->srcset = str_replace('http://' . $domain, '', str_replace('https://' . $domain, '', wp_get_attachment_image_srcset($id)));
+		if (!isset($element->srcset) && $element->parent->tag == "a") {
+			$id = attachment_url_to_postid('https://' . $domain . $element->parent->href);
+			if (!$id) {
+				preg_match('/((\.jpg)|(\.jpeg)|(\.png)|(\.gif)$)/i', $element->parent->href, $match, PREG_OFFSET_CAPTURE);
+				$url = substr_replace($element->parent->href, '-scaled', $match[0][1], 0);
+				$id = attachment_url_to_postid('https://' . $domain . $url);
+			}
+			$element->srcset = wp_get_attachment_image_srcset($id);
+		}
+	    $element->srcset = str_replace('http://' . $domain, '', str_replace('https://' . $domain, '', $element->srcset));
 	}
 	return str_replace("<br/>","<br clear='none'/>", $html);
 } 
