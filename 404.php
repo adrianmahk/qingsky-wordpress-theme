41b5f83cc2d9243613d56ea526784f49e0926012
adrianmahk@qingsky <qingskys@gmail.com>
2022-03-22 19:06:26 +0800

    [404] added redirect link to this site, using curl to get title from old site

--- 404.php
+++ 404.php
@@ -22,26 +22,47 @@ get_header(); ?>
 <div class="no-posts-message">
 找不到相符的結果
 <?php
-	if (strpos($_SERVER['REQUEST_URI'], '.html') > 0) {
+	if (strpos($_SERVER['REQUEST_URI'], '.html') > 0) :
 		echo '<br />註：本站在 2022 年 3 月搬家，搬運仍在進行中，鏈結可能失效，這裡可能有你想找的東西：<br />';
+		
 		$url = 'https://blogger.qingsky.hk' . $_SERVER['REQUEST_URI'];
-		echo '<a href="' . $url . '">' . $url . '</a><br />' ;
-		echo '（此為本站的 Blogger 舊版本的鏈結）';
-	}
-?>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- qingskys_page_body_Blog1_1x1_as -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-6259217392436204"
-     data-ad-slot="4167252325"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-<br />
+		$ch = curl_init($url); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+		$apiResponse = curl_exec($ch);
+		curl_close($ch); 
+		
+		preg_match('/(?<=<title>)(.|\n)*?(?=<\/title>)/i', $apiResponse, $match);
+		$title = $match[0];
+		if ($title):
+			global $wp;
+			$url = urldecode(home_url($wp->request));
+			preg_match('/^.{1,}(?=((?:\/).{1,}(.html$)))/i', $url, $match);
+			$real_url = $match[0] . '/' . str_replace(' ', '-',$title);
+
+			echo '<a href="' . $real_url . '">' . $real_url . '</a><br />' ;
+		
+		// echo '（此為本站的 Blogger 舊版本的鏈結）';
+	?>
+		<script>
+		</script>
+<?php	endif; 
+	else :?>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<!-- qingskys_page_body_Blog1_1x1_as -->
+	<ins class="adsbygoogle"
+		style="display:block"
+		data-ad-client="ca-pub-6259217392436204"
+		data-ad-slot="4167252325"
+		data-ad-format="auto"
+		data-full-width-responsive="true"></ins>
+	<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+	<br />
 <cm>輕鬆一下</cm>
+<?php endif?>
+
+
 </div>
 </div>
 </div>
