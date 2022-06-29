1028faa0c9d3c563440d4328e16c5e73dbf226b5
adrianmahk@qingsky <qingskys@gmail.com>
2022-06-29 18:56:50 +0800

    "

--- functions.php
+++ functions.php
@@ -735,7 +735,8 @@ function update_view_count() {
 		
 		// Detailed version (will be purged by cron)
 		$view_count_table_sql = "CREATE TABLE IF NOT EXISTS `wordpress`.`wp_viewcounts` ( `title` TEXT NULL, `post_id` BIGINT(20) NULL, `view_count` INT(11) NOT NULL DEFAULT '0', `is_valid` BOOLEAN NOT NULL DEFAULT TRUE, `url` VARCHAR(255) NOT NULL , `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , `update_time` TIME NOT NULL DEFAULT CURRENT_TIMESTAMP  , PRIMARY KEY (`url`, `date`)) ENGINE = InnoDB";
-		$view_count_sql = "INSERT INTO `wp_viewcounts` (`post_id`, `post_title`, `post_url`, `view_count`) VALUES(" . get_the_ID() . ", '" . get_the_title() . "', '" . get_permalink() . "', 1) ON DUPLICATE KEY UPDATE `view_count` = `view_count` + 1";
+		$view_count_sql = "INSERT INTO `wp_viewcounts` (`url`, `date`, `update_time`, `view_count`, `is_valid`, `post_id`, `title`) VALUES('". urldecode(home_url($wp->request))."', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, " . (is_404() ? 0 : 1)  . ", " . (is_singular() ?  get_the_ID() : 0) . ", '" . html_entity_decode(wp_get_document_title()) . "') ON DUPLICATE KEY UPDATE `view_count` = `view_count` + 1, `update_time` = CURRENT_TIMESTAMP";
+		// $view_count_sql = "INSERT INTO `wp_viewcounts` (`post_id`, `post_title`, `post_url`, `view_count`) VALUES(" . get_the_ID() . ", '" . get_the_title() . "', '" . get_permalink() . "', 1) ON DUPLICATE KEY UPDATE `view_count` = `view_count` + 1";
 
 		// User region (not working for now)
 		// $user_region = getIpData($_SERVER['REMOTE_ADDR']);
